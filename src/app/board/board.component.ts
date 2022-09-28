73bc7bb5cb4f92d7fa2485d6ec04dcb41d4fca37
Danial Ali <thedanial.ali@gmail.com>
2022-09-27 21:23:04 -0400

    Tic-tac-toe

---
+++ src/app/board/board.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.css']
+})
+export class BoardComponent implements OnInit {
+  squares!: any[];
+  xIsNext: boolean = false;
+  winner: string | undefined;
+  counter = 0;
+  constructor() { }
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+  newGame() {
+    this.counter=0;
+    this.squares = Array(9).fill(null);
+    this.winner = '';
+    this.xIsNext = true;
+  }
+  get player(){
+    return this.xIsNext? 'X':'O';
+  }
+makeMove(idx:number){
+  this.counter++;
+  if(!this.squares[idx]){
+    this.squares.splice(idx,1, this.player);
+    this.xIsNext = !this.xIsNext;
+  }
+  this.winner = this.calculateWinner();
+  if(this.winner){
+    this.winner+=" won the round!";
+  }
+  else if(this.counter>8 && !this.winner){
+    this.winner = "Cat's Game... nobody won :(";
+  }
+}
+calculateWinner(){
+  /*
+  [0,1,2]
+  [3,4,5]
+  [6,7,8]
+  */
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ];
+  for(let i=0;i<lines.length;i++){
+    const[a,b,c] = lines[i];
+    if(this.squares[a] && 
+       this.squares[a] === this.squares[b] &&
+       this.squares[b]===this.squares[c]){
+        return this.squares[a];
+       }
+  }
+  return null;
+}
+}
